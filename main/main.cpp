6e3a85ac6a09c6d50e9d76cfcd210ba1e0e06eb1
Scott Aron Bloom <scott@towel42.com>
2025-09-10 23:28:02 +0100

    Initial checkin

---
+++ main/main.cpp
@@ -0,0 +1,25 @@
+#include "MainWindow/MainWindow.h"
+
+#include <QApplication>
+#include <QMessageBox>
+
+#include "Version.h"
+
+int main( int argc, char ** argv )
+{
+    QApplication appl( argc, argv );
+    Q_INIT_RESOURCE( application );
+
+    appl.setApplicationName( NVersion::APP_NAME );
+    appl.setApplicationVersion( NVersion::getVersionString( true ) );
+    appl.setOrganizationName( NVersion::VENDOR );
+    appl.setOrganizationDomain( NVersion::HOMEPAGE );
+
+    appl.setWindowIcon( QPixmap( ":/resources/finddupe.png" ) );
+
+    CMainWindow * wnd = new CMainWindow;
+    wnd->show();
+    wnd->setWindowTitle( QString( "%1 v%2 - http://%3" ).arg( NVersion::APP_NAME ).arg( NVersion::getVersionString( true ) ).arg( NVersion::HOMEPAGE ) );
+    return appl.exec();
+}
+
