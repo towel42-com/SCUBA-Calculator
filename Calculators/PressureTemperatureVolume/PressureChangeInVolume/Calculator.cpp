4ebd62a36243d249c50f7234f485855098998194
Scott Aron Bloom <scott@towel42.com>
2025-10-09 20:03:38 +0100

    Support for Pressure Change in Volume, and Volume Change in Pressure

--- Calculators/PressureTemperatureVolume/PressureChangeInVolume/Calculator.cpp
+++ Calculators/PressureTemperatureVolume/PressureChangeInVolume/Calculator.cpp
@@ -1,5 +1,6 @@
 #include "Calculator.h"
 #include "VariableInfo.h"
+#include "Utilities.h"
 
 class CALCULATORS_EXPORT CCalculator : public CSCUBACalculator
 {
@@ -17,6 +18,7 @@ public:
     virtual std::list< std::shared_ptr< SVariableInfo > > getMyVariables() const override;
     virtual QString getDefaultFormula() const override;
     virtual QString computeAndGenerateFormula( bool & isBaseFormula ) const override;
+    virtual TVariableInfo customDetermineVariableToUnset( EVariableLoc updateFromSide, QWidget *triggerWidget, bool preDefaultBehavior );
 };
 
 extern "C" CSCUBACalculator *instantiateCalculator()
@@ -38,44 +40,83 @@ TVariableInfoList CCalculator::getMyVariables() const
 {
     return   //
         {
-            //std::make_shared< SVariableInfo >( "volumeDisplaced", tr( "Volume Displaced" ), EVariableType::eVariable, EUnit::eVolume, EVariableLoc::eLHS ),   //
+            std::make_shared< SVariableInfo >( "p1", tr( "Pressure 1" ), EVariableType::eVariable, EUnit::ePressure, EVariableLoc::eLHS ),   //
+            std::make_shared< SVariableInfo >( "v1", tr( "Volume 1" ), EVariableType::eVariable, EUnit::eVolume, EVariableLoc::eLHS ),   //
+            std::make_shared< SVariableInfo >( "p2", tr( "Pressure 2" ), EVariableType::eVariable, EUnit::ePressure, EVariableLoc::eRHS ),   //
+            std::make_shared< SVariableInfo >( "v2", tr( "Volume 2" ), EVariableType::eVariable, EUnit::eVolume, EVariableLoc::eRHS )
         };
+}
+
+TVariableInfo CCalculator::customDetermineVariableToUnset( EVariableLoc updateFromSide, QWidget *triggerWidget, bool preDefaultBehavior )
+{
+    TVariableInfo retVal;
+
+    if ( updateFromSide == EVariableLoc::eLHS )
+    {
+        if ( getVariable( "p1" )->isWidget( triggerWidget ) )
+            retVal = getVariable( "p2" );
+        else if ( getVariable( "v1" )->isWidget( triggerWidget ) )
+            retVal = getVariable( "v2" );
+    }
+    else if ( updateFromSide == EVariableLoc::eRHS )
+    {
+        if ( getVariable( "p2" )->isWidget( triggerWidget ) )
+            retVal = getVariable( "p1" );
+        else if ( getVariable( "v2" )->isWidget( triggerWidget ) )
+            retVal = getVariable( "v1" );
+    }
+    else
+        retVal = CSCUBACalculator::customDetermineVariableToUnset( updateFromSide, triggerWidget, preDefaultBehavior );
+    return retVal;
 }
 
 QString CCalculator::getDefaultFormula() const
 {
     QString formula;
+    formula = R"__(<p1> \times <v1> = <p2> \times <v2>)__";
     return formula;
 }
 
-QString CCalculator::computeAndGenerateFormula( bool & isBaseFormula ) const
+QString CCalculator::computeAndGenerateFormula( bool &isBaseFormula ) const
 {
-    isBaseFormula = true;
-    return {};
-    //if ( !NUtilities::valuesValid( values ) )
-    //    return {};
+    auto p1 = getVariable( "p1" );
+    auto v1 = getVariable( "v1" );
 
-    //auto p1 = values[ 0 ];
-    //auto v1 = values[ 1 ];
-    //auto p2 = values[ 2 ];
-    //auto v2 = values[ 3 ];
+    auto p2 = getVariable( "p2" );
+    auto v2 = getVariable( "v2" );
 
-    //if ( !p1.has_value() )
-    //{
-    //    p1 = ( p2.value() * v2.value() ) / v1.value();
-    //}
-    //else if ( !v1.has_value() )
-    //{
-    //    v1 = ( p2.value() * v2.value() ) / p1.value();
-    //}
-    //else if ( !p2.has_value() )
-    //{
-    //    p2 = ( p1.value() * v1.value() ) / v2.value();
-    //}
-    //else if ( !v2.has_value() )
-    //{
-    //    v2 = ( p1.value() * v1.value() ) / p2.value();
-    //}
-    //return TOptionalDoubleVector( { p1, v1, p2, v2 } );
+    QString formula;
+    bool aOK = numUnsetVariables() == 1;
+    isBaseFormula = false;
+    if ( !aOK )
+    {
+        formula = getDefaultFormula();
+        isBaseFormula = true;
+    }
+    else if ( !p1->has_value() )
+    {
+        // p1 = p2 * (v2/v1);
+        p1->setValue( p2->value() * ( v2->value() / v1->value() ) );
+        formula = tr( R"__(<p1> = <p2> \times \frac{<v2>}{<v1>})__" );
+    }
+    else if ( !p2->has_value() )
+    {
+        // p2 = p1 * (v1/v2);
+        p2->setValue( p1->value() * ( v1->value() / v2->value() ) );
+        formula = tr( R"__(<p2> = <p1> \times \frac{<v1>}{<v2>})__" );
+    }
+    else if ( !v1->has_value() )
+    {
+        // v1 = v2 * (p2/p1)
+        v1->setValue( v2->value() * ( p2->value() / p1->value() ) );
+        formula = tr( R"__(<v1> = <v2> \times \frac{<p2>}{<p1>})__" );
+    }
+    else if ( !v2->has_value() )
+    {
+        // v2 = v1 * (p1/p2)
+        v2->setValue( v1->value() * ( p1->value() / p2->value() ) );
+        formula = tr( R"__(<v2> = <v1> \times \frac{<p1>}{<p2>})__" );
+    }
+
+    return formula;
 }
-
