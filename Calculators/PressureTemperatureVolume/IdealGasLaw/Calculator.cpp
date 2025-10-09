c89bf0fb6c20359cb38f17bdebaf5f96fe963f1e
Scott Aron Bloom <scott@towel42.com>
2025-10-09 04:01:29 +0100

    Support for ideal gas law

--- Calculators/PressureTemperatureVolume/IdealGasLaw/Calculator.cpp
+++ Calculators/PressureTemperatureVolume/IdealGasLaw/Calculator.cpp
@@ -1,5 +1,6 @@
 #include "Calculator.h"
 #include "VariableInfo.h"
+#include "Utilities.h"
 
 class CALCULATORS_EXPORT CCalculator : public CSCUBACalculator
 {
@@ -38,44 +39,59 @@ TVariableInfoList CCalculator::getMyVariables() const
 {
     return   //
         {
-            //std::make_shared< SVariableInfo >( "volumeDisplaced", tr( "Volume Displaced" ), EVariableType::eVariable, EUnit::eVolume, EVariableLoc::eLHS ),   //
+            std::make_shared< SVariableInfo >( "p", tr( "Pressure" ), EVariableType::eVariable, EUnit::ePressure, EVariableLoc::eLHS ),   //
+            std::make_shared< SVariableInfo >( "v", tr( "Volume" ), EVariableType::eVariable, EUnit::eVolume, EVariableLoc::eLHS ),   //
+            std::make_shared< SVariableInfo >( "numMoles", tr( "Number of Moles" ), EVariableType::eVariable, EUnit::eNone, EVariableLoc::eRHS ),   //
+            std::make_shared< SVariableInfo >( "t", tr( "Temperature" ), EVariableType::eVariable, EUnit::eAbsZeroTemperature, EVariableLoc::eRHS ),   //
+            std::make_shared< SVariableInfo >( "idealGasConstant", tr( "Ideal Gas Constant" ), EVariableType::eIdealGasConst, EUnit::eNone, EVariableLoc::eRHS ),   //
+            std::make_shared< SVariableInfo >( "absOffset", tr( "Absolute Temperature Offset" ), EVariableType::eAbsZeroOffsetConst, EUnit::eNone, EVariableLoc::eRHS ),   //
         };
 }
 
 QString CCalculator::getDefaultFormula() const
 {
-    return {};
+    return tr( R"__(<p> \times <v> = <numMoles> \times <idealGasConstant> \times <t>)__" );
 }
 
-QString CCalculator::computeAndGenerateFormula( bool & isBaseFormula ) const
+QString CCalculator::computeAndGenerateFormula( bool &isBaseFormula ) const
 {
-    isBaseFormula = true;
-    return {};
-    //if ( !NUtilities::valuesValid( values ) )
-    //    return {};
+    auto p = getVariable( "p" );
+    auto v = getVariable( "v" );
+    auto numMoles = getVariable( "numMoles" );
+    auto t = getVariable( "t" );
 
-    //auto p = values[ 0 ];
-    //auto v = values[ 1 ];
-    //auto numMoles = values[ 2 ];
-    //auto t = values[ 3 ];
+    QString formula;
+    bool aOK = numUnsetVariables() == 1;
+    isBaseFormula = false;
+    if ( !aOK )
+    {
+        formula = getDefaultFormula();
+        isBaseFormula = true;
+    }
+    else if ( !p->has_value() )
+    {
+        // p = nrt/v
+        p->setValue( numMoles->value() * NUtilities::NConstants::idealGasConstant( imperial() ) * NUtilities::toAbsZeroBasedTemp( imperial(), t->value() ) / v->value() );
+        formula = tr( R"__(<p> = \frac{<numMoles> \times <idealGasConstant> \times (<t> + <absOffset>)}{<v>})__" );
+    }
+    else if ( !v->has_value() )
+    {
+        // v = nrt/p
+        v->setValue( numMoles->value() * NUtilities::NConstants::idealGasConstant( imperial() ) * NUtilities::toAbsZeroBasedTemp( imperial(), t->value() ) / p->value() );
+        formula = tr( R"__(<v> = \frac{<numMoles> \times <idealGasConstant> \times (<t> + <absOffset>)}{<p>})__" );
+    }
+    else if ( !numMoles->has_value() )
+    {
+        // n = pv/rt
+        numMoles->setValue( ( p->value() * v->value() ) / ( NUtilities::NConstants::idealGasConstant( imperial() ) * NUtilities::toAbsZeroBasedTemp( imperial(), t->value() ) ) );
+        formula = tr( R"__(<numMoles> = \frac{<p> \times <v>}{<idealGasConstant> \times (<t> + <absOffset>)})__" );
+    }
+    else if ( !t->has_value() )
+    {
+        // t = pv/nr
+        t->setValue( NUtilities::fromAbsZeroBasedTemp( imperial(), ( p->value() * v->value() ) / ( NUtilities::NConstants::idealGasConstant( imperial() ) * numMoles->value() ) ) );
+        formula = tr( R"__(<t> = (\frac{<p> \times <v>}{<idealGasConstant> \times <numMoles>}) - <absOffset>)__" );
+    }
 
-    //// p*v = numMoles * R * t
-    //if ( !p.has_value() )
-    //{
-    //    p = numMoles.value() * NUtilities::NConstants::idealGasConstant( imperial() ) * NUtilities::toAbsZeroBasedTemp( imperial(), t.value() ) / v.value();
-    //}
-    //else if ( !v.has_value() )
-    //{
-    //    v = numMoles.value() * NUtilities::NConstants::idealGasConstant( imperial() ) * NUtilities::toAbsZeroBasedTemp( imperial(), t.value() ) / p.value();
-    //}
-    //else if ( !numMoles.has_value() )
-    //{
-    //    numMoles = ( p.value() * v.value() ) / ( NUtilities::NConstants::idealGasConstant( imperial() ) * NUtilities::toAbsZeroBasedTemp( imperial(), t.value() ) );
-    //}
-    //else if ( !t.has_value() )
-    //{
-    //    t = NUtilities::fromAbsZeroBasedTemp( imperial(), ( p.value() * v.value() ) / ( NUtilities::NConstants::idealGasConstant( imperial() ) * numMoles.value() ) );
-    //}
-    //return TOptionalDoubleVector( { p, v, numMoles, t } );
+    return formula;
 }
-
