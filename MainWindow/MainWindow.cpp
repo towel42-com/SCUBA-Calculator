784d1f2b4e8751c0b00a48fecb059ad742462c77
Scott Aron Bloom <scott@towel42.com>
2025-10-10 20:09:31 +0100

    Always show the current formula, even if the same as the base formula

--- MainWindow/MainWindow.cpp
+++ MainWindow/MainWindow.cpp
@@ -52,7 +52,6 @@ CMainWindow::CMainWindow( QWidget *parent ) :
     fRenderingEngine = new NTowel42::CQt6MathJax( this );
     fImpl->baseFormulaWidget->setEngine( fRenderingEngine );
     fImpl->currFormulaWidget->setEngine( fRenderingEngine );
-    fImpl->currFormulaWidget->setSubordinateTo( { fImpl->baseFormulaWidget } );
     fImpl->currFormulaValueWidget->setEngine( fRenderingEngine );
     fImpl->currFormulaValueWidget->setSubordinateTo( { fImpl->baseFormulaWidget, fImpl->currFormulaWidget } );
 
