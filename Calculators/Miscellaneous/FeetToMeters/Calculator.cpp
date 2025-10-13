f000e61f5609d3ad18a133e8bb850062ad670130
Scott Aron Bloom <scott@towel42.com>
2025-10-13 19:33:30 +0100

    Fix for feet to meters formula

--- Calculators/Miscellaneous/FeetToMeters/Calculator.cpp
+++ Calculators/Miscellaneous/FeetToMeters/Calculator.cpp
@@ -51,6 +51,7 @@ TVariableInfoList CCalculator::getMyVariables() const
         {
             std::make_shared< CVariableInfo >( "meters", tr( "Length" ), EVariableType::eVariable, EUnit::eNone, EVariableLoc::eLHS ),   //
             std::make_shared< CVariableInfo >( "feet", tr( "Length" ), EVariableType::eVariable, EUnit::eNone, EVariableLoc::eRHS ),   //
+            std::make_shared< CVariableInfo >( EVariableType::eFeetToMetersConst ),   //
             std::make_shared< CVariableInfo >( EVariableType::eMetersToFeetConst ),   //
         } );
 
