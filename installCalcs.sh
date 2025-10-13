def251a430f63596358fcb017bb0c315386595d1
Scott Aron Bloom <scott@towel42.com>
2025-10-14 00:15:32 +0100

    CMake takes care of installing calculators

--- installCalcs.sh
+++
@@ -1,7 +0,0 @@
-#for dir in Debug RelWithDebInfo; do
-for dir in Debug; do
-    rm -rf build/main/$dir/Calculators/*
-    mkdir -p build/main/$dir/Calculators
-    cp build/Calculators/*/*/$dir/*.dll build/main/$dir/Calculators
-    cp build/Calculators/*/*/$dir/*.pdb build/main/$dir/Calculators
-done    
