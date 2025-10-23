775679453c30545196284a17914d0c54f8d62a19
Scott Aron Bloom <scott@towel42.com>
2025-10-23 01:11:18 +0100

    Support for END

---
+++ Calculators/PartialPressure/END/Calculator.cpp
@@ -0,0 +1,87 @@
+#include "Calculator.h"
+#include "Core/VariableInfo.h"
+#include "Core/Utilities.h"
+
+#include <cmath>
+
+class CALCULATORS_EXPORT CCalculator : public CSCUBACalculator
+{
+public:
+    CCalculator() { setObjectName( "END" ); }
+    virtual ~CCalculator() override {}
+    virtual bool isWaterTypeBased() const override { return true; }
+
+    virtual QString myCalculatorName() const override;
+    virtual QStringList myCalculatorPath() const override;
+
+    virtual TVariableInfoList getMyVariables() const override;
+
+    virtual QString myBaseFormula( bool imperial, bool seaWater ) const override;   // for descriptive purposes
+    virtual std::optional< QString > getFormulaForVar( const TConstVariableInfo &unsetVar, bool imperial, bool seaWater ) const override;   // returns the current formula in use
+
+    virtual void computeValueForVar( TVariableInfo &unsetVar ) override;   // updates all values
+};
+
+extern "C" CSCUBACalculator *instantiateCalculator()
+{
+    return new CCalculator;
+}
+
+QString CCalculator::myCalculatorName() const
+{
+    return tr( "Equivalent Narcotic Depth w/Helium (END)" );
+}
+
+QStringList CCalculator::myCalculatorPath() const
+{
+    return { tr( "Partial Pressure Calculations" ) };
+}
+
+TVariableInfoList CCalculator::getMyVariables() const
+{
+    auto retVal =   //
+        TVariableInfoList( {
+            std::make_shared< CVariableInfo >( "depth", tr( "Depth" ), EVariableType::eVariable, EUnit::eDepth, EVariableLoc::eRHS ),   //
+            std::make_shared< CVariableInfo >( "fhe", tr( "Helium Percentage" ), EVariableType::eVariable, EUnit::ePercent, EVariableLoc::eRHS ),   //
+            std::make_shared< CVariableInfo >( "end", tr( "Equivalent Narcotic Depth (END)" ), EVariableType::eVariable, EUnit::eDepth, EVariableLoc::eLHS ),   //
+            std::make_shared< CVariableInfo >( EVariableType::eDepthToSingleATMConst ),   //
+        } );
+    return retVal;
+}
+
+QString CCalculator::myBaseFormula( bool /*imperial*/, bool /*seaWater*/ ) const
+{
+    return QString( R"__(<end> = [(<depth> + <%1>) \times ( 1.0 - <fhe> ) ] - <%1> )__" ).arg( NUtilities::fieldNameForType( EVariableType::eDepthToSingleATMConst ) );
+}
+
+std::optional< QString > CCalculator::getFormulaForVar( const TConstVariableInfo &unsetVar, bool /*imperial*/, bool /*seaWater*/ ) const
+{
+    if ( unsetVar->name() == "end" )
+        return myBaseFormula( false, false );
+    else if ( unsetVar->name() == "fhe" )
+        return QString( R"__(<fhe> =1.0 - \frac{<end> + %1}{<depth> + <%1>} )__" ).arg( NUtilities::fieldNameForType( EVariableType::eDepthToSingleATMConst ) );
+    else if ( unsetVar->name() == "depth" )
+        return QString( R"__(<depth> = \frac{<end> + %1}{1.0 - <fhe>} - <%1>))__" ).arg( NUtilities::fieldNameForType( EVariableType::eDepthToSingleATMConst ) );
+    return {};
+}
+
+void CCalculator::computeValueForVar( TVariableInfo &unsetVar )
+{
+    auto depth = getVariable( "depth" );
+    auto fhe = getVariable( "fhe" );
+    auto end = getVariable( "end" );
+
+    auto depthToSingleATM = NUtilities::NConstants::depthToSingleAtmosphere( imperial(), seaWater() );
+    if ( unsetVar == end )
+    {
+        end->setValue( ( ( depth->value() + depthToSingleATM ) * ( 1.0 - fhe->value() ) ) - depthToSingleATM );
+    }
+    else if ( unsetVar == fhe )
+    {
+        fhe->setValue( 1.0 - ( end->value() + depthToSingleATM ) / ( depth->value() + depthToSingleATM ) );
+    }
+    else if ( unsetVar == depth )
+    {
+        depth->setValue( ( ( end->value() + depthToSingleATM ) / ( 1.0 - fhe->value() ) ) - depthToSingleATM );
+    }
+}
