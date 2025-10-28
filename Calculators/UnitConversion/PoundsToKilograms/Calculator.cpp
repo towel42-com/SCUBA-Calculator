7fdef0e15f13c1c1d5f2cb94a39efd984c0b800c
Scott Aron Bloom <scott@towel42.com>
2025-10-28 22:11:44 +0100

    AddToCalculator list generates CalculatorDef.h which defines the group name and the project name

--- Calculators/UnitConversion/PoundsToKilograms/Calculator.cpp
+++ Calculators/UnitConversion/PoundsToKilograms/Calculator.cpp
@@ -1,4 +1,5 @@
 #include "Calculator.h"
+#include "CalculatorDef.h"
 #include "Core/VariableInfo.h"
 #include "Core/Utilities.h"
 
@@ -15,6 +16,9 @@ public:
 
     virtual QStringList myCalculatorPath() const override;
     virtual QStringList myReversedCalculatorPath() const override;
+
+    virtual QString calculatorProjectName() const override { return kProjectName; }
+    virtual QString calculatorGroupName() const override { return kGroupName; }
 
     virtual bool showUnits() const { return false; }
 
