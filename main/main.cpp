1b7094de50c827211af385e6303e9296fcfae150
Scott Aron Bloom <scott@towel42.com>
2025-12-02 01:19:55 +0000

    latest version files

--- main/main.cpp
+++ main/main.cpp
@@ -12,7 +12,7 @@ int main( int argc, char **argv )
     NTowel42::CMathJaxQt6::initResources();
 
     appl.setApplicationName( NVersion::APP_NAME );
-    appl.setApplicationVersion( NVersion::getVersionString( true ) );
+    appl.setApplicationVersion( NVersion::getVersionString( true, false ) );
     appl.setOrganizationName( NVersion::VENDOR );
     appl.setOrganizationDomain( NVersion::HOMEPAGE );
 
@@ -20,6 +20,6 @@ int main( int argc, char **argv )
 
     CMainWindow *wnd = new CMainWindow;
     wnd->show();
-    wnd->setWindowTitle( QString( "%1 v%2 - http://%3" ).arg( NVersion::APP_NAME ).arg( NVersion::getVersionString( true ) ).arg( NVersion::HOMEPAGE ) );
+    wnd->setWindowTitle( QString( "%1 v%2 - http://%3" ).arg( NVersion::APP_NAME ).arg( NVersion::getVersionString( true, false ) ).arg( NVersion::HOMEPAGE ) );
     return appl.exec();
 }
